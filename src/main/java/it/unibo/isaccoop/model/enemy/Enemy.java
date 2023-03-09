374403dab83bd9d5df07eb1708043ab0816a5c6c
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-03-09 11:05:28 +0100

    Remove useless Hittator interface

--- src/main/java/it/unibo/isaccoop/model/enemy/Enemy.java
+++ src/main/java/it/unibo/isaccoop/model/enemy/Enemy.java
@@ -1,6 +1,20 @@
 package it.unibo.isaccoop.model.enemy;
 
 /***/
-public class Enemy {
+public class Enemy implements EnemyActions {
+
+
+
+    @Override
+    public void hit() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void move() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
