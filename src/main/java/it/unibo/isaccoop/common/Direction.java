bf1dbf1ee73cddc831e7992b2ac88c3b3b25b53a
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-09 09:47:27 +0100

    fix errors in Direction.java

--- src/main/java/it/unibo/isaccoop/common/Direction.java
+++ src/main/java/it/unibo/isaccoop/common/Direction.java
@@ -2,16 +2,22 @@ package it.unibo.isaccoop.common;
 
 import java.util.Random;
 
-public enum Direction{
+/**
+ * Enum that models the concept of direction.
+ */
+public enum Direction {
     UP(0,-1), DOWN(0,1), RIGHT(1,0), LEFT(-1,0);
 
-    int x;
-    int y;
-    Direction(int x , int y) {
+    final int x;
+    final int y;
+    Direction(final int x, final int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @return a random direction
+     */
     public static Direction getRandomDir() {
         return Direction.values()[new Random().nextInt(Direction.values().length)];
     }
