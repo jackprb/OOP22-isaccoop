97794d04969bfa5cfd11703725b314118a8e2f4e
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-09 10:35:33 +0100

    add Level.java

---
+++ src/main/java/it/unibo/isaccoop/model/Level.java
@@ -0,0 +1,22 @@
+package it.unibo.isaccoop.model;
+
+import java.util.Map;
+import org.apache.commons.lang3.tuple.Pair;
+
+/**
+ * Interface to model a level, which contains many {@link Room}.
+ */
+public interface Level {
+
+    /**
+     * inserts the specified rooms map in this level.
+     * 
+     * @param roomsMap the room map to be added to this level
+     */
+    void putRoomMap(Map<Pair<Integer, Integer>, Room> roomsMap);
+
+    /**
+     * @return the map of all rooms in this level and their coordinates in room grid
+     */
+    Map<Pair<Integer, Integer>, Room> getRooms();
+}
