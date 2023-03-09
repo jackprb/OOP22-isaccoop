db6961cd50b933cec36ec99eccb2f3434db3fe8e
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-03-09 09:28:54 +0100

    Continue to add basic AI enemy representation

---
+++ src/main/java/it/unibo/isaccoop/model/enemy/EnemyWeaponType.java
@@ -0,0 +1,17 @@
+package it.unibo.isaccoop.model.enemy;
+
+/**
+ * Enum to represent the enemy weapon type.
+ * */
+public enum EnemyWeaponType {
+
+    /**
+     * Non shooting type.
+     * */
+    NON_SHOOTING,
+
+    /**
+     * Shooting type.
+     * */
+    SHOOTING
+}
---
+++ src/main/java/it/unibo/isaccoop/model/enemy/Hittator.java
@@ -0,0 +1,11 @@
+package it.unibo.isaccoop.model.enemy;
+
+/***/
+public interface Hittator {
+
+    /**
+     * Method to be called after the striker hit the player.
+     * */
+    void onHit();
+
+}
