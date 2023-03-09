be44a978c65351ddb5308e9ae569c09e2aca260d
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-03-09 12:12:44 +0100

    fixed some PMD and checkstyle errors

--- src/main/java/it/unibo/isaccoop/model/Shop.java
+++ src/main/java/it/unibo/isaccoop/model/Shop.java
@@ -11,24 +11,22 @@ import org.apache.commons.lang3.tuple.Pair;
  * */
 public class Shop {
     private final Map<Pair<Integer, Integer>, Integer> powerUPs = new HashMap<>();
-    private static final int N_POWER_UP = 3;
     /**
      * @param powerUPs
      * Generate of N_POWER_UP in the PowerUP list.
      */
-    public Shop(Map<Pair<Integer, Integer>, Integer> powerUPs) {
-        this.powerUPs.putAll(powerUPs);;
+    public Shop(final Map<Pair<Integer, Integer>, Integer> powerUPs) {
+        this.powerUPs.putAll(powerUPs);
     }
-    
+ 
     /**
      * 
      * @param heroPos
      */
-    public void getItem(Pair<Integer, Integer> heroPos) {
-        var powerUP = this.powerUPs.get(heroPos);
+    public void useItem(final Pair<Integer, Integer> heroPos) {
+        //final var powerUP = this.powerUPs.get(heroPos);
         //powerUP.interact();
         this.powerUPs.remove(heroPos);
     }
-    
-    
+
 }
