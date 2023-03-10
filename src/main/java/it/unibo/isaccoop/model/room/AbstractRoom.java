1afeb8e744f51a37b93409ef5eb1456c7123fe5f
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-10 17:14:42 +0100

    add AbstractRoom.java

---
+++ src/main/java/it/unibo/isaccoop/model/room/AbstractRoom.java
@@ -0,0 +1,34 @@
+package it.unibo.isaccoop.model.room;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
+
+import it.unibo.isaccoop.model.common.MapElementImpl;
+import it.unibo.isaccoop.model.common.RoomType;
+
+/**
+ * Abstract {@link Room} that implements the main methods.
+ */
+public abstract class AbstractRoom extends MapElementImpl implements Room {
+
+    private final List<Door> doors = new LinkedList<>();
+    private final RoomType roomType;
+    
+    public AbstractRoom(final int id, final int width, final int height, 
+            final Pair<Integer, Integer> coord, final RoomType roomType) {
+        super(id, width, height, coord);
+        this.roomType = roomType;
+    }
+
+    @Override
+    public List<Door> getDoors() {
+        return Collections.unmodifiableList(this.doors);
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return this.roomType;
+    }    
+}
