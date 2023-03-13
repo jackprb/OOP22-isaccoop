0e9608907118f3365bc649cd95390e303eb806ca
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-03-13 15:03:24 +0100

    Add BossCreator and EnemyCreator

--- src/main/java/it/unibo/isaccoop/model/ai/AIEnemy.java
+++ src/main/java/it/unibo/isaccoop/model/ai/AIEnemy.java
@@ -1,15 +1,21 @@
 package it.unibo.isaccoop.model.ai;
 
+import java.util.List;
+
+import it.unibo.isaccoop.model.enemy.Enemy;
+
 /***/
 public interface AIEnemy {
 
     /**
-     * Method to spawn enemies into the room linked to AI impl.
+     * Method to attach enemies to AI.
+     *
+     * @param enemies input enemies to attach to AI as {@link List}
      * */
-    void createEnemies();
+    void attachEnemies(List<Enemy> enemies);
 
     /**
-     * Method to perform enemies actions (move and hit) into the room linked to AI impl.
+     * Method to perform enemies actions (move and hit) into the room linked to AI implementation.
      * */
     void updateEnemies();
 
---
+++ src/main/java/it/unibo/isaccoop/model/ai/BossCreator.java
@@ -0,0 +1,5 @@
+package it.unibo.isaccoop.model.ai;
+
+public class BossCreator {
+
+}
