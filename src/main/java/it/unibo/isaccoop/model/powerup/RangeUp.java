389b32f170a9597da5e685a8f8396e0c6c4af46c
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-03-14 11:24:53 +0100

    Add javadoc in powerup package

--- src/main/java/it/unibo/isaccoop/model/powerup/RangeUp.java
+++ src/main/java/it/unibo/isaccoop/model/powerup/RangeUp.java
@@ -2,12 +2,17 @@ package it.unibo.isaccoop.model.powerup;
 
 import it.unibo.isaccoop.model.player.PlayerStat;
 
-/***/
+/**
+ * Represents the power up range.
+ * */
 public class RangeUp extends PowerUp {
     private static final float RANGE_SUPER_UP = 2;
     private static final float RANGE_UP = 1;
 
-    /***/
+    /**
+     * Increase the player's range.
+     * @param p reference to player.
+     * */
     @Override
     public void interact(final PlayerStat p) {
         if (super.isSuperItem()) {
--- src/main/java/it/unibo/isaccoop/model/powerup/TearsUp.java
+++ src/main/java/it/unibo/isaccoop/model/powerup/TearsUp.java
@@ -2,13 +2,18 @@ package it.unibo.isaccoop.model.powerup;
 
 import it.unibo.isaccoop.model.player.PlayerStat;
 
-/***/
+/**
+ * Represents the power up tears.
+ * */
 public class TearsUp extends PowerUp {
 
     private static final int TEARS_SUPER_UP = 2;
     private static final int TEARS_UP = 1;
 
-    /***/
+    /**
+     *  Increase the player's speed.
+     *  @param p reference to player.
+     * */
     @Override
     public void interact(final PlayerStat p) {
         if (super.isSuperItem()) {
