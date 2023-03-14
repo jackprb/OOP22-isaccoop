cdcb0568d2a67df642254e3a857c96f0da76a5e6
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-03-14 11:07:32 +0100

    Add javadoc in item package

--- src/main/java/it/unibo/isaccoop/model/item/Coin.java
+++ src/main/java/it/unibo/isaccoop/model/item/Coin.java
@@ -3,14 +3,14 @@ package it.unibo.isaccoop.model.item;
 import it.unibo.isaccoop.model.player.PlayerStat;
 
 /**
- *
+ * Represents a coin in the game.
  * */
 public class Coin extends AbstractItem {
     private static final int COIN_UP = 1;
 
     /**
      *  Method for interacting with player money.
-     *  @param p reference to player
+     *  @param p reference to player.
      * */
     @Override
     public void interact(final PlayerStat p) {
