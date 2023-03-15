df63de61a97f6c9774657041a232310127e38691
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-03-15 15:02:29 +0100

    resolve conflict in Creator

--- src/main/java/it/unibo/isaccoop/model/common/Creator.java
+++ src/main/java/it/unibo/isaccoop/model/common/Creator.java
@@ -4,13 +4,6 @@ import java.util.List;
 
 /**
  * Functional interface that manages the creation of items and enemies.
-<<<<<<< HEAD
- * @param <E>
- * */
-public interface Creator<E> {
-    /**
-     * @return List<E> list of objects
-=======
  *
  * @param <E> type of {@link Creator}
  * */
@@ -19,7 +12,6 @@ public interface Creator<E> {
      * Method to create a {@link List} of <E> type objects.
      *
      * @return {@link List} of <E> type with created objects
->>>>>>> b2e444e8e7d3b019434ac8f7684b55f3f59037e9
      * */
     List<E> create();
 }
