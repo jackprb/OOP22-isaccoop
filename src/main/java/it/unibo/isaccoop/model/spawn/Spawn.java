ed0869c57f651b76cb50eac6df49318332bf99f4
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-03-16 17:28:01 +0100

    Pull from master

--- src/main/java/it/unibo/isaccoop/model/spawn/Spawn.java
+++ src/main/java/it/unibo/isaccoop/model/spawn/Spawn.java
@@ -1,14 +1,18 @@
 package it.unibo.isaccoop.model.spawn;
 
+import java.util.List;
+
+import it.unibo.isaccoop.model.common.MapElement;
+
 /**
  * 
  * @param <E>
  */
-public interface Spawn<E> {
+public interface Spawn {
 
     /**
      * Method that set the position of the element inside the room.
      * @param elementToSpawn
      */
-    void setPosition(E elementToSpawn);
+    void setPosition(List<MapElement> elementsToSpawn);
 }
---
+++ src/main/java/it/unibo/isaccoop/model/spawn/SpawnRandom.java
@@ -0,0 +1,19 @@
+package it.unibo.isaccoop.model.spawn;
+
+import java.util.List;
+
+import it.unibo.isaccoop.model.common.MapElement;
+
+public class SpawnRandom implements Spawn {
+
+    public SpawnRandom() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void setPosition(List<MapElement> elementsToSpawn) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
