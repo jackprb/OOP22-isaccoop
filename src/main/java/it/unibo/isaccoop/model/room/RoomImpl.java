bbbcf66553e41797aa1378656bcb0ea0178199b3
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-18 20:57:22 +0100

    update RoomImpl with new methods implementation

--- src/main/java/it/unibo/isaccoop/model/room/RoomImpl.java
+++ src/main/java/it/unibo/isaccoop/model/room/RoomImpl.java
@@ -1,7 +1,5 @@
 package it.unibo.isaccoop.model.room;
 
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -10,16 +8,20 @@ import it.unibo.isaccoop.model.ai.AIEnemy;
 import it.unibo.isaccoop.model.common.MapElementImpl;
 import it.unibo.isaccoop.model.common.Point2D;
 import it.unibo.isaccoop.model.common.RoomType;
+import it.unibo.isaccoop.model.item.Item;
+import it.unibo.isaccoop.model.powerup.PowerUp;
 
 /**
  * Implementation of {@link Room}.
  */
 public final class RoomImpl extends MapElementImpl implements Room {
 
-    private final List<Door> doors = new LinkedList<>();
+    //private final List<Door> doors = new LinkedList<>();
     private final RoomType roomType;
     private final Optional<AIEnemy> roomAi;
-
+    private final Optional<List<Item>> items;
+    private final Optional<List<PowerUp>> powerups;
+    
     /**
      * Use {@link RoomFactory} to create a new {@link Room}.
      * @param width horizontal dimension of this room
@@ -30,19 +32,21 @@ public final class RoomImpl extends MapElementImpl implements Room {
      * @param roomAI the AiEnemy for this room
      */
     public RoomImpl(final int width, final int height,
-            final Point2D coord, final List<Door> doors, final RoomType roomType,
-            final AIEnemy roomAI) {
+            final Point2D coord, /*final List<Door> doors,*/ final RoomType roomType,
+            final Optional<AIEnemy> roomAI, final Optional<List<Item>> items, 
+            final Optional<List<PowerUp>> powerups) {
         super(width, height, coord);
-        this.doors.addAll(doors);
         this.roomType = roomType;
-        this.doors.addAll(doors);
-        this.roomAi = Optional.of(roomAI);
+        //this.doors.addAll(doors);
+        this.roomAi = roomAI;
+        this.items = items;
+        this.powerups = powerups;
     }
 
-    @Override
+    /*@Override
     public List<Door> getDoors() {
         return Collections.unmodifiableList(this.doors);
-    }
+    }*/
 
     @Override
     public RoomType getRoomType() {
@@ -55,6 +59,16 @@ public final class RoomImpl extends MapElementImpl implements Room {
     }
 
     @Override
+    public Optional<List<Item>> getItems() {
+        return this.items;
+    }
+    
+    @Override
+    public Optional<List<PowerUp>> getPowerUps() {
+        return this.powerups;
+    }
+    
+    @Override
     public int hashCode() {
         final int prime = 31;
         int result = super.hashCode();
@@ -64,16 +78,16 @@ public final class RoomImpl extends MapElementImpl implements Room {
 
     @Override
     public boolean equals(final Object obj) {
-        if (this == obj) {
+        if (this == obj) {            
             return true;
         }
-        if (!super.equals(obj)) {
+        if (!super.equals(obj)) {            
             return false;
         }
         if (getClass() != obj.getClass()) {
             return false;
         }
         final RoomImpl other = (RoomImpl) obj;
-        return Objects.equals(doors, other.doors) && roomType == other.roomType;
+        return roomType == other.roomType;
     }
 }
