834fb503c07ee32248125e36b3597e20f7ad1713
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-03-20 16:31:37 +0100

    Changed implementation of bounding box

--- src/main/java/it/unibo/isaccoop/model/boundingbox/BoundingBox.java
+++ src/main/java/it/unibo/isaccoop/model/boundingbox/BoundingBox.java
@@ -8,12 +8,11 @@ import it.unibo.isaccoop.model.common.Point2D;
 public interface BoundingBox {
 
     /**
-     * Check for collisions.
-     * 
-     * @param p2
-     * @param p3
-     * @return true if a collision occurs
+     * Check for collisions of two bounding box.
+     * @param p
+     * @param radius
+     * @return true if collision occurs
      */
-    boolean isCollidingWith(Point2D p2, Point2D p3);
+    boolean isCollidingWith(Point2D p, double radius);
 
 }
