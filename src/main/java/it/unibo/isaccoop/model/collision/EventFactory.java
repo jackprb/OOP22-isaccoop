65cf074a913cee0f0d2f4d59e1cba0f96b03b5fb
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-03-21 11:31:42 +0100

    Complete Event and EventFactory interfaces

--- src/main/java/it/unibo/isaccoop/model/collision/EventFactory.java
+++ src/main/java/it/unibo/isaccoop/model/collision/EventFactory.java
@@ -1,5 +1,32 @@
 package it.unibo.isaccoop.model.collision;
 
+import it.unibo.isaccoop.model.item.Item;
+import it.unibo.isaccoop.model.player.PlayerStat;
+
+/***/
 public interface EventFactory {
+
+    /**
+     * Get a new event when a pickup is made.
+     *
+     * @param target item picked up
+     * @return event created
+     * */
+    Event getItemPickUpEvent(Item target);
+
+    /**
+     * Get a new event when an enemy is shot.
+     *
+     * @param player player who fired
+     * @return event created
+     * */
+    Event getEnemyShot(PlayerStat player);
+
+    /**
+     * Get a new event when a player is shot.
+     *
+     * @return event created
+     * */
+    Event getEnemyHitEvent();
 
 }
