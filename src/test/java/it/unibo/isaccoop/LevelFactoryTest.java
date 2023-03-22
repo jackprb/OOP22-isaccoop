12ab8c6e2f07db2dfe3ce208428cc5b2b48a38b4
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-22 20:00:56 +0100

    refactor first test in LevelFactoryTest

--- src/test/java/it/unibo/isaccoop/LevelFactoryTest.java
+++ src/test/java/it/unibo/isaccoop/LevelFactoryTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.isaccoop.model.common.RoomType;
+import it.unibo.isaccoop.model.room.LevelFactory;
 import it.unibo.isaccoop.model.room.LevelFactoryImpl;
+import it.unibo.isaccoop.model.room.Room;
 
 /**
  * RoomFactory test.
@@ -14,16 +16,22 @@ class LevelFactoryTest {
     private static final int NUMBER_OF_ROOMS = 10;
     private static final int SINGLE_ROOM_COUNT = 1;
 
+    private LevelFactory lvlFactory = new LevelFactoryImpl();
+
     @Test 
-    void testCreateLevel() {
-        final var levelFactory = new LevelFactoryImpl();
-        final var lvl = levelFactory.createLevel(NUMBER_OF_ROOMS);
+    void testRoomCreation() {
+        final var lvl = this.lvlFactory.createLevel(NUMBER_OF_ROOMS);
         final var rooms = lvl.getRooms();
         // check if a just created level has the correct number of rooms
         assertTrue(rooms.size() == NUMBER_OF_ROOMS);
         // and if it is not complete
         assertFalse(lvl.isComplete());
+    }
 
+    @Test
+    void testRoomTypeCount() {
+        final var lvl = this.lvlFactory.createLevel(NUMBER_OF_ROOMS);
+        final var rooms = lvl.getRooms();
         // in a level, there must be ONLY ONE START, ONE SHOP, ONE TREASURE and ONE BOSS room
         assertTrue(rooms.stream()
                 .filter(r -> r.getRoomType() == RoomType.START)
@@ -50,4 +58,21 @@ class LevelFactoryTest {
         + "roomAi: " + r.getRoomAI() + "\n"
         + "roomtype: " + r.getRoomType()));*/
     }
+
+    @Test 
+    void testPlayerLocationInLevel() {
+        // when a level is created, the player must be in the START room ONLY
+        final var lvl = this.lvlFactory.createLevel(NUMBER_OF_ROOMS);
+        final var rooms = lvl.getRooms();
+
+        // considering only the START rooms (there will be only one), and in that one
+        // where will be the player
+        assertTrue(rooms.stream().filter(r -> r.getRoomType() == RoomType.START)
+                .allMatch(r -> r.getPlayer().isPresent()));
+        // considering the other rooms, the player will not be in any of them
+        assertTrue(rooms.stream().filter(r -> r.getRoomType() != RoomType.START)
+                .allMatch(r -> r.getPlayer().isEmpty()));
+    }
+
+
 }
