5f54442fe698efe61e6938b62a30da6650300615
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-03-22 10:19:07 +0100

    Added implementation of SpawnOrdered and modified SpawnRandom

--- src/main/java/it/unibo/isaccoop/model/spawn/SpawnOrdered.java
+++ src/main/java/it/unibo/isaccoop/model/spawn/SpawnOrdered.java
@@ -4,25 +4,32 @@
 package it.unibo.isaccoop.model.spawn;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import it.unibo.isaccoop.model.common.MapElement;
 import it.unibo.isaccoop.model.common.Point2D;
 
 /**
  *
+ * 
  */
 public class SpawnOrdered implements Spawn {
 
     /**
-     *
+     * Method that sets the positions of the elements inside the room, in an ordered way.
+     * @param elementsToSpawn
+     * @param width of room
+     * @param height of room
      */
     @Override
-    public void setPosition(final List<MapElement> elementsToSpawn) {
-        elementsToSpawn.forEach(e -> {
-          //fissiamo gli item al centro della room, heigh/2 e controlliamo la
-          //size della lista e poi /2 se 1 elem e /4 se 3 elem
-            e.setCoords(new Point2D(0, 0));
-        });
+    public void setPosition(final List<MapElement> elementsToSpawn, final int width, final int height) {
+        if (elementsToSpawn.size() == 1) {
+            elementsToSpawn.get(0).setCoords(new Point2D(Double.valueOf(width / 2), Double.valueOf(height / 2)));
+        } else {
+            Stream.iterate(0, x -> x + 1).limit(elementsToSpawn.size()).
+            forEach(i -> elementsToSpawn.get(i).setCoords(new Point2D(Double.valueOf(width / 2),
+                    Double.valueOf((height / (elementsToSpawn.size() + 1)) * (i + 1)))));
+        }
     }
 
 }
