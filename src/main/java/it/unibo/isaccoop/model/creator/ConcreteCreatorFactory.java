1025996a39d80a7dc80ce2f10fe597510744bd84
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-03-23 12:01:59 +0100

    Implement create enemies into concrete creator factory

--- src/main/java/it/unibo/isaccoop/model/creator/ConcreteCreatorFactory.java
+++ src/main/java/it/unibo/isaccoop/model/creator/ConcreteCreatorFactory.java
@@ -1,21 +1,31 @@
 package it.unibo.isaccoop.model.creator;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import it.unibo.isaccoop.model.enemy.Enemy;
+import it.unibo.isaccoop.model.enemy.NonShootingEnemy;
+import it.unibo.isaccoop.model.enemy.ShootingEnemy;
 import it.unibo.isaccoop.model.item.Item;
 import it.unibo.isaccoop.model.powerup.PowerUp;
 
 public class ConcreteCreatorFactory implements CreatorFactory{
 
+    private final static int MAX_ENEMIES = 5;
+
     @Override
     public Creator<Enemy> createEnemies() {
-        // TODO Auto-generated method stub
-        return null;
+        return () -> Stream.iterate(0, i -> i + 1)
+                .limit(ThreadLocalRandom.current().nextInt(ConcreteCreatorFactory.MAX_ENEMIES) + 1)
+                .map(i -> ThreadLocalRandom.current().nextBoolean() ? new NonShootingEnemy() : new ShootingEnemy())
+                .collect(Collectors.toList());
     }
 
     @Override
     public Creator<Enemy> createBoss() {
-        // TODO Auto-generated method stub
-        return null;
+        return () -> List.of();
     }
 
     @Override
