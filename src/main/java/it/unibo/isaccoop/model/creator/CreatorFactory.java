17ce44b1b1d0b70b2359fcaf39dc1ef662b30011
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-03-23 17:28:41 +0100

    Fix ConcreteCreatorFactory and CreatorFactory checkstyle error

--- src/main/java/it/unibo/isaccoop/model/creator/CreatorFactory.java
+++ src/main/java/it/unibo/isaccoop/model/creator/CreatorFactory.java
@@ -8,35 +8,35 @@ import it.unibo.isaccoop.model.powerup.PowerUp;
 public interface CreatorFactory {
 
     /**
-     * Method to get an enemy creator
+     * Method to get an enemy creator.
      *
      * @return an enemy creator
      * */
     Creator<Enemy> createEnemies();
 
     /**
-     * Method to get a boss creator
+     * Method to get a boss creator.
      *
      * @return a boss creator
      * */
     Creator<Enemy> createBoss();
 
     /**
-     * Method to get a shop power up creator
+     * Method to get a shop power up creator.
      *
      * @return a shop power up creator
      * */
     Creator<PowerUp> createShopPowerUps();
 
     /**
-     * Method to get a treasure power up creator
+     * Method to get a treasure power up creator.
      *
      * @return a treasure power up creator
      * */
     Creator<PowerUp> createTreasurePowerUps();
 
     /**
-     * Method to get an items creator
+     * Method to get an items creator.
      *
      * @return an items creator
      * */
