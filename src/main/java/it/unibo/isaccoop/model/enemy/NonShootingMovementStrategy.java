4c57944d30b86c12fa0f485caeb96c634d0b8f86
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2023-03-24 18:13:02 +0100

    add MovementStrategy for enemy

---
+++ src/main/java/it/unibo/isaccoop/model/enemy/NonShootingMovementStrategy.java
@@ -0,0 +1,15 @@
+package it.unibo.isaccoop.model.enemy;
+
+import it.unibo.isaccoop.model.common.Point2D;
+import it.unibo.isaccoop.model.common.Vector2D;
+
+/***/
+public class NonShootingMovementStrategy implements MovementStrategy {
+
+    /***/
+    @Override
+    public Point2D move(final Point2D enemyPosition, final Point2D playerPosition) {
+        final Vector2D movementVector = playerPosition.sub(playerPosition).mul(1.0 / NonShootingEnemy.getSpeed());
+        return enemyPosition.sum(movementVector);
+    }
+}
