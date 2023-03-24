1e7005386fb62b02e828463cade4560040dbb8ea
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-03-24 14:40:57 +0100

    complete getCollisionPlayerShotChecker

--- src/main/java/it/unibo/isaccoop/model/collision/CollisionCheckFactoryImpl.java
+++ src/main/java/it/unibo/isaccoop/model/collision/CollisionCheckFactoryImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import it.unibo.isaccoop.model.common.AbstractMapElement.ElementsRadius;
 import it.unibo.isaccoop.model.common.MapElement;
+import it.unibo.isaccoop.model.enemy.Enemy;
 import it.unibo.isaccoop.model.item.Item;
 import it.unibo.isaccoop.model.player.Player;
 /**
@@ -12,34 +13,28 @@ import it.unibo.isaccoop.model.player.Player;
  *
  */
 public class CollisionCheckFactoryImpl implements CollisionCheckFactory {
-    /**
-     *
-     */
+
     @Override
     public CollisionCheck getCollisionWithItemChecker(final Player p, final List<MapElement> i) {
         return room -> i.stream()
                 .filter(elem -> elem.getBox().isCollidingWith(p.getCoords(), elem.getCoords(), ElementsRadius.PLAYER.getValue()))
                 .forEach(e -> room.notifyEvent(new ConcreteEventFactory().getItemPickUpEvent((Item) e)));
     }
-    /**
-     *
-     */
+
     @Override
     public CollisionCheck getCollisionPlayerShotChecker(final Player p, final List<MapElement> i) {
         return room -> i.stream()
-                .filter(elem -> elem.getBox().isCollidingWith(p.getCoords(), elem.getCoords(), ElementsRadius.PLAYER.getValue()));
+                .filter(elem -> p.getShot().stream()
+                        .anyMatch(shot -> shot.getBox().isCollidingWith(shot.getCoords(), elem.getCoords(), ElementsRadius.ENEMY.getValue())))
+                        .forEach(e -> room.notifyEvent(new ConcreteEventFactory().getEnemyShot((Enemy) e)));
     }
-    /**
-     *
-     */
+
     @Override
     public CollisionCheck getCollisionWithEnemyChecker(final Player p, final List<MapElement> i) {
         return room -> i.stream()
                 .filter(elem -> elem.getBox().isCollidingWith(p.getCoords(), elem.getCoords(), ElementsRadius.PLAYER.getValue()));
     }
-    /**
-     *
-     */
+
     @Override
     public CollisionCheck getCollisionWithEnemyShotChecker(final Player p, final List<MapElement> i) {
         return room -> i.stream()
