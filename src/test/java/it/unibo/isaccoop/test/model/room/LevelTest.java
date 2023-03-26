dca533da18c0a0cf500a51acd4dd0d904bd24f90
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-26 12:03:03 +0200

    add empty files LevelTest and RoomTest

---
+++ src/test/java/it/unibo/isaccoop/test/model/room/LevelTest.java
@@ -0,0 +1,24 @@
+package it.unibo.isaccoop.test.model.room;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * RoomFactory test.
+ * */
+class LevelTest {
+    
+    @BeforeEach
+    void setUp() {
+        
+    }
+
+    @Test 
+    void test1() {
+        
+    }
+
+    @Test
+    void test2() {
+    }
+}
