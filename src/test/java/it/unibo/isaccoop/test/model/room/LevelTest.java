eae5c0b25eb16bb61780068a5fee0c76abccb564
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-26 12:54:28 +0200

    add other tests in LevelTest

--- src/test/java/it/unibo/isaccoop/test/model/room/LevelTest.java
+++ src/test/java/it/unibo/isaccoop/test/model/room/LevelTest.java
@@ -1,6 +1,9 @@
 package it.unibo.isaccoop.test.model.room;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -22,39 +25,43 @@ import it.unibo.isaccoop.model.room.RoomFactoryImpl;
 class LevelTest {
     private static final int NUMBER_OF_STANDARD_ROOMS = 10;
     
-    private Level level;
     private final RoomFactory roomFactory = new RoomFactoryImpl();
-    
-    @BeforeEach
-    void setUp() {
-        this.level = new LevelImpl();    
-    }
+    private final Level level = new LevelImpl();
+    private List<Room> rooms = new LinkedList<>();
+    private List<Room> rooms2 = new LinkedList<>();
 
     @Test 
     void testPutRooms() {
-        final List<Room> rooms = new LinkedList<>();
-        final List<Room> rooms2 = new LinkedList<>();
+        // generate due lists of Room
         for (final RoomType roomType: RoomType.values()) {
-            rooms.add(this.roomFactory.buildRoomOfType(roomType, new Point2D(roomType.ordinal(), 0)));
+            this.rooms.add(this.roomFactory.buildRoomOfType(roomType, new Point2D(roomType.ordinal(), 0)));
         }
         for (int i = 0; i < NUMBER_OF_STANDARD_ROOMS; i++) {
-            rooms2.add(this.roomFactory.buildStandardRoom(new Point2D(i, 0)));
+            this.rooms2.add(this.roomFactory.buildStandardRoom(new Point2D(i, 0)));
         }
         this.level.putRooms(rooms); // the level has no rooms -> OK
         
         // the level already has rooms (set in previous line) -> throw exception
-        assertThrows(IllegalStateException.class, () -> this.level.putRooms(rooms2)); 
+        assertThrows(IllegalStateException.class, () -> this.level.putRooms(this.rooms2)); 
     }
 
     @Test
     void testGetRooms() {
+        // the 
+        assertEquals(this.level.getRooms(), this.rooms);
     }
     
     @Test
     void testIsLevelComplete() {
+        // a just created level cannot be already complete
+        assertFalse(this.level.isComplete());
     }
 
     @Test
     void testGetStartRoom() {
+        final Room startRoom = this.level.getRooms().stream()
+                .filter(r -> r.getRoomType() == RoomType.START)
+                .findFirst().get();
+        assertEquals(this.level.getStartRoom(), startRoom);
     }
 }
