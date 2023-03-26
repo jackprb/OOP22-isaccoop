244bf670bb9c01eaf235e02965dacfb58e31a6aa
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-26 13:27:43 +0200

    minor changes in LevelFactoryImpl

--- src/main/java/it/unibo/isaccoop/model/room/LevelFactoryImpl.java
+++ src/main/java/it/unibo/isaccoop/model/room/LevelFactoryImpl.java
@@ -16,7 +16,7 @@ public final class LevelFactoryImpl implements LevelFactory {
 
     private int numberOfRooms;
     private final List<Pair<Integer, Integer>> roomCoords = new LinkedList<>();
-    // ogni livello deve avere almeno 5 room, una per ogni tipo
+    // each level must have at least 5 rooms, one for each RoomType
     private static final int MIN_NUMBER_OF_ROOMS = RoomType.values().length;
 
     /**
@@ -29,7 +29,7 @@ public final class LevelFactoryImpl implements LevelFactory {
     @Override
     public Level createLevel(final int numberOfRooms) {
         if (numberOfRooms < MIN_NUMBER_OF_ROOMS) {
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException("a level must have at least " + MIN_NUMBER_OF_ROOMS + " rooms");
         }
         this.numberOfRooms = numberOfRooms;
         setRoomCoordinates();
@@ -45,7 +45,7 @@ public final class LevelFactoryImpl implements LevelFactory {
      * for the rooms in this level.
      */
     private void setRoomCoordinates() {
-        //posiz iniziale per posizionare stanze nel livello
+        // initial position for the first room of the level
         Pair<Integer, Integer> roomPos = new ImmutablePair<>(0, 0);
 
         for (int i = 0; i < this.numberOfRooms; i++) {
@@ -57,7 +57,7 @@ public final class LevelFactoryImpl implements LevelFactory {
     }
 
     /**
-     * get a new coordinate from the current coordinate coord, along the specified direction dir.
+     * Get a new coordinate from the current coordinate coord, along the specified direction dir.
      * @param coord the current coordinate from which calculate the new coordinate
      * @param dir the direction along which the coordinate has to ben calculated
      * @return the new coordinate calculated from coordinate coord along direction dir
@@ -86,10 +86,10 @@ public final class LevelFactoryImpl implements LevelFactory {
         for (int i = 0; i < this.roomCoords.size(); i++) {
             final var coord = pair2point2D(this.roomCoords.get(rooms.size()));
             if (i < RoomType.values().length) {
-                // crea una room di ogni tipo (BOSS, SHOP, TREASURE, START, STANDARD)
+                // create a room for each RoomType (BOSS, SHOP, TREASURE, START, STANDARD)
                 rooms.add(rFactory.buildRoomOfType(RoomType.values()[i], coord));
             } else {
-                // quelle rimanenti devono essere di tipo STANDARD
+                // the remaining ones must be STANDARD
                 rooms.add(rFactory.buildStandardRoom(coord));
             }
         }
