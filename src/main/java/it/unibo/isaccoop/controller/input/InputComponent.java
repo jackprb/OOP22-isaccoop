7a7f813977a13adf9928bd952892e0fac9b576c7
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-03-28 09:07:40 +0200

    Added InputComponent and InputController skeleton

---
+++ src/main/java/it/unibo/isaccoop/controller/input/InputComponent.java
@@ -0,0 +1,16 @@
+package it.unibo.isaccoop.controller.input;
+
+import it.unibo.isaccoop.model.common.MapElement;
+
+/**
+ * 
+ *
+ */
+public interface InputComponent {
+
+    /**
+     * 
+     * @param e
+     */
+    void update(MapElement e);
+}
---
+++ src/main/java/it/unibo/isaccoop/controller/input/InputController.java
@@ -0,0 +1,12 @@
+package it.unibo.isaccoop.controller.input;
+
+public interface InputController {
+
+    boolean isMoveUp();
+
+    boolean isMoveDown();
+
+    boolean isMoveLeft();
+
+    boolean isMoveRight();
+}
