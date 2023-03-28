cf1ff3d3d6d3b1fd8ffee89aec6f0342abe904b6
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-28 13:04:53 +0200

    add new empty test file RoomTest

---
+++ src/test/java/it/unibo/isaccoop/test/model/room/RoomTest.java
@@ -0,0 +1,57 @@
+package it.unibo.isaccoop.test.model.room;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.isaccoop.model.common.Point2D;
+import it.unibo.isaccoop.model.common.RoomType;
+import it.unibo.isaccoop.model.room.Level;
+import it.unibo.isaccoop.model.room.LevelFactoryImpl;
+import it.unibo.isaccoop.model.room.LevelImpl;
+import it.unibo.isaccoop.model.room.Room;
+import it.unibo.isaccoop.model.room.RoomFactory;
+import it.unibo.isaccoop.model.room.RoomFactoryImpl;
+
+/**
+ * Room test.
+ * */
+class RoomTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testGetRoomType() {
+    }
+
+    @Test
+    void testGetRoomAI() {
+    }
+
+    @Test
+    void testGetItems() {
+    }
+
+    @Test
+    void testGetPowerUps() {
+        
+    }
+
+    @Test
+    void testGetPlayer() {   
+    }
+
+    @Test
+    void testGetEnemies() {   
+    }
+
+    @Test
+    void testIsComplete() {
+    }
+}
