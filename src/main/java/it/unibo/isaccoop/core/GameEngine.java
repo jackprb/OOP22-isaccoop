fd842169e01fbfe281dd556de383ea93406eccc0
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-03-29 17:10:02 +0200

    first implement of initgame and getControllers in gameEngineImpl

--- src/main/java/it/unibo/isaccoop/core/GameEngine.java
+++ src/main/java/it/unibo/isaccoop/core/GameEngine.java
@@ -3,9 +3,18 @@ package it.unibo.isaccoop.core;
  *
  *
  */
+
+import it.unibo.isaccoop.controller.input.InputController;
+
 public interface GameEngine {
     /**
      *
      */
     void initGame();
+    /**
+     *
+     * @param name
+     * @return
+     */
+    InputController getController(String name);
 }
--- src/main/java/it/unibo/isaccoop/core/GameEngineImpl.java
+++ src/main/java/it/unibo/isaccoop/core/GameEngineImpl.java
@@ -1,16 +1,32 @@
 package it.unibo.isaccoop.core;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.awt.event.KeyEvent.*;
+
+import it.unibo.isaccoop.controller.input.InputController;
+import it.unibo.isaccoop.controller.input.KeyboardInputController;
+
 /**
  *
  *
  */
 public class GameEngineImpl implements GameEngine {
-    /**
-     *
-     */
+
+    private Map<String, InputController> controllers = new HashMap<>();
+
     @Override
     public void initGame() {
-        // TODO Auto-generated method stub
+        this.controllers.put("keyMove", new KeyboardInputController(VK_W, VK_S, VK_A, VK_D));
+        this.controllers.put("keyShot", new KeyboardInputController(VK_UP, VK_DOWN, VK_LEFT, VK_RIGHT));
+    }
 
+    @Override
+    public InputController getController(String name) {
+        return this.controllers.get(name);
     }
 
 }
