11223f64ab1dd1d96f4227bb456132d62cea9803
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-29 12:36:16 +0200

    add constructor in LevelFactoryImpl

--- src/main/java/it/unibo/isaccoop/model/room/LevelFactoryImpl.java
+++ src/main/java/it/unibo/isaccoop/model/room/LevelFactoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.commons.lang3.tuple.Pair;
 
 import it.unibo.isaccoop.model.common.RoomType;
+import it.unibo.isaccoop.model.player.Player;
 
 /**
  * Implementation of {@link LevelFactory}.
@@ -13,13 +14,22 @@ public final class LevelFactoryImpl implements LevelFactory {
 
     // each level must have at least 5 rooms, one for each RoomType
     private static final int MIN_NUMBER_OF_ROOMS = RoomType.values().length;
+    private final Player player;
+    
+    /**
+     * Get the player.
+     * @param player the player to be put inside this level
+     */
+    public LevelFactoryImpl(final Player player) {
+        this.player = player;
+    }
 
     @Override
     public Level createLevel(final int numberOfRooms) {
         if (numberOfRooms < MIN_NUMBER_OF_ROOMS) {
             throw new IllegalArgumentException("a level must have at least " + MIN_NUMBER_OF_ROOMS + " rooms");
         }
-        final LevelFactoryUtils lvlFactoryUtils = new LevelFactoryUtils();
+        final LevelFactoryUtils lvlFactoryUtils = new LevelFactoryUtils(this.player);
         final List<Pair<Integer, Integer>> roomCoords = new LinkedList<>();
         roomCoords.addAll(lvlFactoryUtils.generateRoomCoordinates(numberOfRooms));
 
