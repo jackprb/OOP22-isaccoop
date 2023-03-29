9917df51a7d95eaf253d0a0fb1f1c882bd558696
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-29 11:12:17 +0200

    add checks to prevent wrong room creation

--- src/main/java/it/unibo/isaccoop/model/room/RoomFactoryImpl.java
+++ src/main/java/it/unibo/isaccoop/model/room/RoomFactoryImpl.java
@@ -9,64 +9,94 @@ import it.unibo.isaccoop.model.player.Player;
  */
 public final class RoomFactoryImpl implements RoomFactory {
 
+    private static final int MIN_MAX_ROOM_DIMENSIONS = 200;
+    private static final String ALREADY_GENERATED_ALL_ROOMS = "you have already generated all the required rooms";
+    private static final String START_ROOM_MUST_BE_FIRST = "the START room must be the FIRST to be generated";
+    private static final String BOSS_ROOM_MUST_BE_LAST = "the BOSS room must be the LAST to be generated";
+
     private final int width;
     private final int height;
-    private static final int MIN_MAX_ROOM_DIMENSIONS = 200;
-    private final Player player;
+    private Player player;
+    private int roomCount = 0;
+    private boolean alreadyCreatedBossRoom = false;
+    private boolean alreadyCreatedStartRoom = false;
+    private final int totalNumberOfRooms;
 
     /**
      * Constructor. No parameters needed.
      */
-    public RoomFactoryImpl() {
+    public RoomFactoryImpl(final int totalNumberOfRooms) {
         this.width = MIN_MAX_ROOM_DIMENSIONS;
         this.height = MIN_MAX_ROOM_DIMENSIONS;
-        this.player = new Player();
+        this.totalNumberOfRooms = totalNumberOfRooms;
     }
 
     @Override
     public Room buildStartRoom(final Point2D coordInsideLevel) {
-        return new RoomBuilder.Builder(this.width, this.height)
-                .roomType(RoomType.START)
-                .putCoord(coordInsideLevel)
-                .putPlayer(player)
-                .build();
+        if (this.roomCount == 0 && !this.alreadyCreatedStartRoom) {            
+            incrementRoomCount();
+            this.alreadyCreatedStartRoom = true;
+            return new RoomBuilder.Builder(this.width, this.height)
+                    .roomType(RoomType.START)
+                    .putCoord(coordInsideLevel)
+                    .putPlayer(player)
+                    .build();
+        }
+        throw new IllegalStateException(START_ROOM_MUST_BE_FIRST);
     }
 
     @Override
     public Room buildStandardRoom(final Point2D coordInsideLevel) {
-        return new RoomBuilder.Builder(this.width, this.height)
-                .roomType(RoomType.STANDARD)
-                .putCoord(coordInsideLevel)
-                .putEnemies()
-                .putItems()
-                .build();
+        if (this.roomCount < this.totalNumberOfRooms) {            
+            incrementRoomCount();
+            return new RoomBuilder.Builder(this.width, this.height)
+                    .roomType(RoomType.STANDARD)
+                    .putCoord(coordInsideLevel)
+                    .putEnemies()
+                    .putItems()
+                    .build();
+        }
+        throw new IllegalStateException(ALREADY_GENERATED_ALL_ROOMS);
     }
 
     @Override
     public Room buildShopRoom(final Point2D coordInsideLevel) {
-        return new RoomBuilder.Builder(this.width, this.height)
-                .roomType(RoomType.SHOP)
-                .putCoord(coordInsideLevel)
-                .putPowerUps()
-                .build();
+        if (this.roomCount < this.totalNumberOfRooms) {            
+            incrementRoomCount();
+            return new RoomBuilder.Builder(this.width, this.height)
+                    .roomType(RoomType.SHOP)
+                    .putCoord(coordInsideLevel)
+                    .putPowerUps()
+                    .build();
+        }
+        throw new IllegalStateException(ALREADY_GENERATED_ALL_ROOMS);
     }
 
     @Override
     public Room buildBossRoom(final Point2D coordInsideLevel) {
-        return new RoomBuilder.Builder(this.width, this.height)
-                .roomType(RoomType.BOSS)
-                .putCoord(coordInsideLevel)
-                .putEnemies()
-                .build();
+        if (this.roomCount == this.totalNumberOfRooms - 1 && !this.alreadyCreatedBossRoom) { 
+            incrementRoomCount();
+            this.alreadyCreatedBossRoom = true;
+            return new RoomBuilder.Builder(this.width, this.height)
+                    .roomType(RoomType.BOSS)
+                    .putCoord(coordInsideLevel)
+                    .putEnemies()
+                    .build();
+        }
+        throw new IllegalStateException(BOSS_ROOM_MUST_BE_LAST);
     }
 
     @Override
     public Room buildTreasureRoom(final Point2D coordInsideLevel) {
-        return new RoomBuilder.Builder(this.width, this.height)
-                .roomType(RoomType.TREASURE)
-                .putCoord(coordInsideLevel)
-                .putPowerUps()
-                .build();
+        if (this.roomCount < this.totalNumberOfRooms) {            
+            incrementRoomCount();
+            return new RoomBuilder.Builder(this.width, this.height)
+                    .roomType(RoomType.TREASURE)
+                    .putCoord(coordInsideLevel)
+                    .putPowerUps()
+                    .build();
+        }
+        throw new IllegalStateException(ALREADY_GENERATED_ALL_ROOMS);
     }
 
     @Override
@@ -85,5 +115,13 @@ public final class RoomFactoryImpl implements RoomFactory {
         default:
             throw new IllegalArgumentException("Incorrect roomType value: " + roomType);
         }
+    }
+
+    /**
+     * Increments the room count. Useful to check if a specified room can be generated in that index.
+     * (START room must alway be the first, BOSS room the last)
+     */
+    private void incrementRoomCount() {
+        this.roomCount++;
     }
 }
