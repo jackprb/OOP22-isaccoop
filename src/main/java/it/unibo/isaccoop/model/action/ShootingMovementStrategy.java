ed5b198d10da4661cd6f563c8661063953fea727
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-03-31 14:49:52 +0200

    Add boundary collision check into shooting movement strategy

--- src/main/java/it/unibo/isaccoop/model/action/ShootingMovementStrategy.java
+++ src/main/java/it/unibo/isaccoop/model/action/ShootingMovementStrategy.java
@@ -2,6 +2,7 @@ package it.unibo.isaccoop.model.action;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import it.unibo.isaccoop.model.boundingbox.RectBoundingBox;
 import it.unibo.isaccoop.model.common.Point2D;
 import it.unibo.isaccoop.model.common.Vector2D;
 import it.unibo.isaccoop.model.enemy.ShootingEnemy;
@@ -9,12 +10,25 @@ import it.unibo.isaccoop.model.enemy.ShootingEnemy;
 /***/
 public class ShootingMovementStrategy implements MovementStrategy {
 
+    private RectBoundingBox roomBoundingBix;
+
+    public ShootingMovementStrategy(final RectBoundingBox roomBoundingBox) {
+        this.roomBoundingBix = roomBoundingBox;
+    }
+
     /***/
     @Override
     public Point2D move(final Point2D enemyPosition, final Point2D playerPosition) {
-        final Vector2D moveVector = new Vector2D(
+        var newPos = new Point2D(0, 0);
+
+        do {
+            newPos = new Point2D(enemyPosition.getX(), enemyPosition.getY());
+            Vector2D moveVector = new Vector2D(
                 ThreadLocalRandom.current().nextDouble(-ShootingEnemy.getSpeed(), ShootingEnemy.getSpeed()),
                 ThreadLocalRandom.current().nextDouble(-ShootingEnemy.getSpeed(), ShootingEnemy.getSpeed()));
-        return enemyPosition.sum(moveVector);
+            newPos.sum(moveVector);
+        } while(!(newPos.getX() < this.roomBoundingBix.getWidth() && newPos.getY() < this.roomBoundingBix.getHeight()));
+
+        return newPos;
     }
 }
