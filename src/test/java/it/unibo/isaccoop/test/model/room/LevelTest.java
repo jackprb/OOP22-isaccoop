b88c400f122ce63e4d054322daddaf0af70c4d45
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-31 12:39:09 +0200

    refactor LevelTest

--- src/test/java/it/unibo/isaccoop/test/model/room/LevelTest.java
+++ src/test/java/it/unibo/isaccoop/test/model/room/LevelTest.java
@@ -3,10 +3,8 @@ package it.unibo.isaccoop.test.model.room;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import java.util.LinkedList;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,21 +14,18 @@ import it.unibo.isaccoop.model.room.Level;
 import it.unibo.isaccoop.model.room.LevelFactoryImpl;
 import it.unibo.isaccoop.model.room.LevelImpl;
 import it.unibo.isaccoop.model.room.Room;
-import it.unibo.isaccoop.model.room.RoomFactory;
 import it.unibo.isaccoop.model.room.RoomFactoryImpl;
 
 /**
- * Level test.
+ * {@link Level} test.
  * */
 class LevelTest {
-    private static final int NUMBER_OF_ROOMS = 10;
 
     // variables for local level
-    private final RoomFactory roomFactory = new RoomFactoryImpl(NUMBER_OF_ROOMS);
     private final Level localLevel = new LevelImpl();
     private final List<Room> otherRoomList = new LinkedList<>();
 
-    // variable to store data generate from LevelFactoryImpl
+    // variable to store data generated from LevelFactoryImpl
     private List<Room> roomListFromFactory = new LinkedList<>();
 
     @BeforeEach
@@ -47,7 +42,8 @@ class LevelTest {
     void testPutRooms() {
         // generate another List<Room>, made of 5 rooms, one for each RoomType
         for (final RoomType roomType: RoomType.values()) {
-            this.otherRoomList.add(this.roomFactory.buildRoomInProperOrder(new Point2D(roomType.ordinal(), 0)));
+            this.otherRoomList.add(new RoomFactoryImpl(RoomType.values().length)
+                    .buildRoomInProperOrder(new Point2D(roomType.ordinal(), 0)));
         }
 
         // the level already has rooms (set in method setUp()) -> throw exception
