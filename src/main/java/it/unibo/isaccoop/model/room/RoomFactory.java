0af1cbbaf635a6abf0be7cfa0be138fab961b06c
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-31 10:54:12 +0200

    improve javadoc in RoomFactory

--- src/main/java/it/unibo/isaccoop/model/room/RoomFactory.java
+++ src/main/java/it/unibo/isaccoop/model/room/RoomFactory.java
@@ -9,46 +9,56 @@ import it.unibo.isaccoop.model.common.RoomType;
 public interface RoomFactory {
 
     /** 
+     * Build a {@link RoomType#START} room.
      * @param coordInsideLevel the coordinate of this room inside the level
-     * @return an empty room where the protagonist starts the game.
+     * @return the room where the player starts the game.
      */
     Room buildStartRoom(Point2D coordInsideLevel);
 
     /** 
+     * Build a {@link RoomType#STANDARD} room.
      * @param coordInsideLevel the coordinate of this room inside the level
-     * @return a room with enemies, obstacles and doors.
+     * @return a room with enemies and items.
      */
     Room buildStandardRoom(Point2D coordInsideLevel);
 
     /** 
+     * Build a {@link RoomType#SHOP} room.
      * @param coordInsideLevel the coordinate of this room inside the level
      * @return a room where you can ONLY buy powerups using the collected money.
      */
     Room buildShopRoom(Point2D coordInsideLevel);
 
     /** 
+     * Build a {@link RoomType#BOSS} room.
      * @param coordInsideLevel the coordinate of this room inside the level
      * @return the room with the final boss
      */
     Room buildBossRoom(Point2D coordInsideLevel);
 
     /** 
+     * Build a {@link RoomType#TREASURE} room.
      * @param coordInsideLevel the coordinate of this room inside the level
-     * @return a room where you can find a free powerup
+     * @return the room where you can find a free powerup
      */
     Room buildTreasureRoom(Point2D coordInsideLevel);
 
     /** 
-     * @param roomType type of the room to be created
+     * Build a room of the given {@link RoomType}.
+     * @param roomType the type of the room to be created
      * @param coordInsideLevel the coordinate of this room inside the level
-     * @return a room where you can find a free powerup
+     * @return a room of the given {@link RoomType}
      */
     Room buildRoomOfType(RoomType roomType, Point2D coordInsideLevel);
 
     /**
+     * Build a room in the proper order, that is, each time this method is called, will return a room of
+     * the proper {@link RoomType}. <br><br>The FIRST will be a {@link RoomType#START} room, 
+     * <br>the LAST will be a {@link RoomType#BOSS}, <br>the others will be created randomly among 
+     * {@link RoomType#SHOP} (only ONE), <br>{@link RoomType#TREASURE} (only ONE) and 
+     * <br>{@link RoomType#STANDARD} (all the remaing ones).
      * @param coordInsideLevel the coordinate of this room inside the level
-     * @return a room of the proper {@link RoomType}: will return a START room as the first one, 
-     * a BOSS room as the last, and other rooms will be created randomly among SHOP, TREASURE and STANDARD
+     * @return a room of the proper {@link RoomType}
      */
     Room buildRoomInProperOrder(Point2D coordInsideLevel);
 }
