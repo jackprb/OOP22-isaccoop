ddf407fc5e22209e1712eaa638ef9b18f885402c
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-03-31 12:50:28 +0200

    add new test file LevelControllerTest

---
+++ src/test/java/it/unibo/isaccoop/test/model/room/LevelControllerTest.java
@@ -0,0 +1,60 @@
+package it.unibo.isaccoop.test.model.room;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.isaccoop.model.room.Level;
+
+/**
+ * {@link Level} test.
+ * */
+class LevelControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test 
+    void testGetCurrentLevel() {
+    }
+
+    @Test
+    void testGetRoomsOfCurrentLevel() {
+    }
+
+    @Test
+    void testGetNumberOfRooms() {
+    }
+
+    @Test
+    void testGetPlayerRoomCoord() {
+    }
+
+    @Test
+    void testGetPlayerRoom() {
+    }
+
+    @Test
+    void testGetPlayer() {
+    }
+
+    @Test
+    void testIsRoomComplete() {
+    }
+
+    @Test
+    void testIsCurrentLevelComplete() {
+    }
+
+    @Test
+    void testAreAllLevelsComplete() {
+    }
+
+    @Test
+    void testGetAccessibleRooms() {
+    }
+
+    @Test
+    void testMoveToRoom() {
+    }
+}
