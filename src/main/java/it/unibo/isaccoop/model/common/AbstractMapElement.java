17f28f9db6e4c415846c8091443ac82968a0b287
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-06 09:58:22 +0200

    fix checkstyle errors

--- src/main/java/it/unibo/isaccoop/model/common/AbstractMapElement.java
+++ src/main/java/it/unibo/isaccoop/model/common/AbstractMapElement.java
@@ -8,12 +8,15 @@ import it.unibo.isaccoop.model.boundingbox.BoundingBox;
 import it.unibo.isaccoop.model.boundingbox.CircleBoundingBox;
 import it.unibo.isaccoop.model.boundingbox.RectBoundingBox;
 
-/***/
+/**
+ * 
+ */
 public abstract class AbstractMapElement implements MapElement {
 
     private Point2D coords;
     private BoundingBox box;
     private GraphicsComponent graphicComponent;
+    private static final double FIXED_INITIAL_POSITION = 5.0;
 
     /**
      *
@@ -68,7 +71,7 @@ public abstract class AbstractMapElement implements MapElement {
      *
      * @param coords initial coords
      * @param elemRadius based on the type of the element
-     * */
+     */
     public AbstractMapElement(final Point2D coords, final ElementsRadius elemRadius) {
         this.coords = coords;
         this.box = new CircleBoundingBox(elemRadius.getValue());
@@ -77,7 +80,7 @@ public abstract class AbstractMapElement implements MapElement {
     /**
      * Constructor for {@link AbstractMapElement} with fixed initial position.
      * @param elemRadius based on the type of the element
-     * */
+     */
     public AbstractMapElement(final ElementsRadius elemRadius) {
         this.coords = new Point2D(0.0, 0.0);
         this.box = new CircleBoundingBox(elemRadius.getValue());
@@ -86,10 +89,10 @@ public abstract class AbstractMapElement implements MapElement {
     /**
      * Constructor for {@link AbstractMapElement} with fixed initial position.
      * @param elemRadius based on the type of the element
-     * @param gr
+     * @param gr the {@link GraphicsComponent} for this {@link MapElement}
      * */
     public AbstractMapElement(final ElementsRadius elemRadius, final GraphicsComponent gr) {
-        this.coords = new Point2D(5.0, 5.0);
+        this.coords = new Point2D(FIXED_INITIAL_POSITION, FIXED_INITIAL_POSITION);
         this.box = new CircleBoundingBox(elemRadius.getValue());
         this.graphicComponent = gr;
     }
@@ -124,7 +127,10 @@ public abstract class AbstractMapElement implements MapElement {
         this.coords = new Point2D(coords.getX(), coords.getY());
     }
 
-    /***/
+    /**
+     * 
+     * @param gr
+     */
     protected void setGraphicsComponents(final GraphicsComponent gr) {
         this.graphicComponent = gr;
     }
@@ -146,11 +152,17 @@ public abstract class AbstractMapElement implements MapElement {
         graphicComponent.update(this, g);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(coords);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(final Object obj) {
         if (this == obj) {
@@ -163,7 +175,7 @@ public abstract class AbstractMapElement implements MapElement {
             return false;
         }
         final AbstractMapElement other = (AbstractMapElement) obj;
-        return Double.compare(coords.getX(), other.coords.getX()) == 0 &&
-                Double.compare(coords.getY(), other.coords.getY()) == 0;
+        return Double.compare(coords.getX(), other.coords.getX()) == 0 
+                && Double.compare(coords.getY(), other.coords.getY()) == 0;
     }
 }
