4d331adfac30f6bf9226628244d16d5493059598
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-04-07 15:54:03 +0200

    Fixed the spawn of MapElements

--- src/main/java/it/unibo/isaccoop/model/spawn/SpawnRandom.java
+++ src/main/java/it/unibo/isaccoop/model/spawn/SpawnRandom.java
@@ -3,6 +3,7 @@ package it.unibo.isaccoop.model.spawn;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import it.unibo.isaccoop.model.boundingbox.CircleBoundingBox;
 import it.unibo.isaccoop.model.common.MapElement;
 import it.unibo.isaccoop.model.common.Point2D;
 
@@ -21,8 +22,10 @@ public class SpawnRandom implements Spawn {
     @Override
     public void setPosition(final List<MapElement> elementsToSpawn, final int width, final int height) {
         elementsToSpawn.forEach(e -> {
-            e.setCoords(new Point2D(ThreadLocalRandom.current().nextDouble(Double.valueOf(width)),
-                    ThreadLocalRandom.current().nextDouble(Double.valueOf(height))));
+            e.setCoords(new Point2D(ThreadLocalRandom.current()
+                    .nextDouble(((CircleBoundingBox) e.getBox()).getRadius() + 1 , width - ((CircleBoundingBox) e.getBox()).getRadius() - 1),
+                    ThreadLocalRandom.current()
+                    .nextDouble(((CircleBoundingBox) e.getBox()).getRadius() + 1 , height - ((CircleBoundingBox) e.getBox()).getRadius() - 1)));
         });
 
     }
