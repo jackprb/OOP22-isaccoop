5f0a93ff7bc7db1f064d589f1138de4cf3e10cdb
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-08 11:20:08 +0200

    refactor code to fix spotbugs errors

--- src/main/java/it/unibo/isaccoop/graphics/HelpGUI.java
+++ src/main/java/it/unibo/isaccoop/graphics/HelpGUI.java
@@ -5,10 +5,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -17,6 +17,7 @@ import javax.swing.JTextArea;
  */
 public class HelpGUI extends AbstractGUIFrame {
 
+    private static final long serialVersionUID = -5636279696678208920L;
     private static final Logger LOGGER = Logger.getLogger(SwingScene.class.getName());
     private static final String TITLE = "Isaccoop User Guide";
     private static final String RES_PATH = "it/unibo/isaccoop/help/";
@@ -24,50 +25,41 @@ public class HelpGUI extends AbstractGUIFrame {
     private static final String NEWLINE = System.lineSeparator();
     private static final int TEXTAREA_COLS = 60;
     private static final int TEXTAREA_ROWS = 10;
+    private static final String UTF8 = "UTF-8";
 
     /**
      * Constructor. No parameters required.
      * Creates a GUI to show the game help.
      */
-    protected HelpGUI() {
-        super();
-        super.setTitle(TITLE);
+    protected HelpGUI() throws IOException {
+        super(TITLE);
 
         final JButton btnClose = new JButton("Close");
         final JTextArea textArea = new JTextArea(TEXTAREA_ROWS, TEXTAREA_COLS);
 
         // if exists, load the file "help.txt" from the resources folder as a stream<String> 
         // and put it in a textarea
-        final InputStream in = ClassLoader.getSystemResourceAsStream(RES_PATH + "help.txt");
-        if (in != null) {
-            try {
-                final BufferedReader br = new BufferedReader(new InputStreamReader(in));
-                final Stream<String> linesStream = br.lines();
+        final InputStream in = Objects.requireNonNull(
+                ClassLoader.getSystemResourceAsStream(RES_PATH + "help.txt"));
+        Stream<String> lines;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in, UTF8))) {
+            lines = br.lines();
 
-                final StringBuilder strBuilder = new StringBuilder();
-                linesStream.forEach(s -> strBuilder.append(s + NEWLINE));
-                textArea.setText(strBuilder.toString());
-                br.close();
-            } catch (IOException ex) {
-                LOGGER.severe(ex.getMessage());
-            }
-        } else {
-            // if the file does not exist, put an alternate message in textarea
+            final StringBuilder strBuilder = new StringBuilder();
+            lines.forEach(s -> strBuilder.append(s + NEWLINE));
+            textArea.setText(strBuilder.toString());
+            br.close();
+        } catch (IOException ex) {
+            LOGGER.severe(ex.getMessage());
             textArea.setText(CANNOT_FIND_HELPFILE);
         }
 
-        // get the main BorderLayout
-        final JFrame frame = super.getJFrame();
-        // add the textArea, where to display the help
-        frame.add(new JScrollPane(textArea), BorderLayout.CENTER);
-
+        // add the textArea to the main BorderLayout, where to display the help
+        super.addElementToMainPanel(new JScrollPane(textArea), BorderLayout.CENTER);
+        // add the button to close the window to the main BorderLayout
+        super.addElementToMainPanel(btnClose, BorderLayout.SOUTH);
         btnClose.addActionListener(l -> {
-            this.getJFrame().setVisible(false);
+            this.dispose();
         });
-        frame.add(btnClose, BorderLayout.SOUTH);
-    }
-
-    @Override
-    public void updateView() {
     }
 }
