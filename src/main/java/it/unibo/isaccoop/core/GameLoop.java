25bb9c55ae97c0a9b4aaa337f51611c5b2869cc2
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-08 11:47:45 +0200

    fix checkstyle violations

--- src/main/java/it/unibo/isaccoop/core/GameLoop.java
+++ src/main/java/it/unibo/isaccoop/core/GameLoop.java
@@ -4,9 +4,9 @@ import it.unibo.isaccoop.model.room.Level;
 
 /**
  * Interface representing the game loop.
- *
  */
 public interface GameLoop {
+
     /**
      * Method that starts the main game loop: input, update, render.
      */
@@ -16,18 +16,20 @@ public interface GameLoop {
      * Get if game loop is in pause or not.
      *
      * @return if game loop is in pause
-     * */
+     */
     boolean isPause();
 
     /**
      * Set game loop pause state.
      *
      * @param isPause new game loop pause state
-     * */
+     */
     void setPause(boolean isPause);
 
     /**
-     *  Method to get the level handled by the game loop
-     * */
+     *  Method to get the level handled by the game loop.
+     *
+     *  @return the level handled by the game loop
+     */
     Level getLevel();
 }
